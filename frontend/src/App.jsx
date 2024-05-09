c5e38d8c26147ba7696d46eb1899bc61d3d4d153
Wilson Del Canto <wilsondelcanto.redes@gmail.com>
2024-05-08 22:35:13 -0400

    chore: Update landing page banner color and add "Hola Grupo 4" to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import './App.css'
-
 function App() {
 
   return (
     <>
       <h1>
-        Hola
+        Hola Grupo 4
       </h1>
     </>
   )
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
+    darkmode: 'media',
     extend: {},
   },
   plugins: [],
-}
-
+};
