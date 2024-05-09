7bdd8d34be3f7424db382ac312e04be208213898
Wilson Del Canto <wilsondelcanto.redes@gmail.com>
2024-05-08 22:43:56 -0400

    Update landing page banner color and improve App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ function App() {
 
   return (
     <>
-      <h1>
-        Hola Grupo 4
-      </h1>
+    <h1 className="text-3xl font-bold underline">
+      Hello, Group 4!
+    </h1>
     </>
   )
 }
