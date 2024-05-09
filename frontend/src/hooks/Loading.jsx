fb31b7ef84f9d12847178707aa97c7ceca705859
Andrés Pistone <vegtta89@gmail.com>
2024-05-08 23:56:43 -0300

    error al pasar a la rama main

--- frontend/src/hooks/Loading.jsx
+++
@@ -1,5 +0,0 @@
-const Loading = () => {
-  return <div className="loading">Cargando...</div>;
-};
-
-export default Loading;
--- frontend/src/routes/routes.js
+++
@@ -1,4 +0,0 @@
-export const routes = {
-  home: '/',
-  notFound: '*'
-};
