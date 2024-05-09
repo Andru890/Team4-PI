e7881b4f36f4784690206d15cc6704442bf9f2be
Wilson Del Canto <wilsondelcanto.redes@gmail.com>
2024-05-08 22:16:42 -0400

    chore: Add Vite configuration and necessary files for React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Hola
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+const NotFound = () => {
+  return (
+    <div>
+      <img
+        src="/images/NotFound.png"
+        alt="Not Found"
+        className="notFoundImage"
+      />
+    </div>
+  );
+};
+
+export default NotFound;
