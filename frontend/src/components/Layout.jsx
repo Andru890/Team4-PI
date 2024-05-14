de5a16faafbc58e82fcdbf581d2f500cb6eebc6c
wdelcant <wilsondelcanto.redes@gmail.com>
2024-05-14 00:40:19 -0400

    refactor: Update Layout component with new routes and components

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -11,57 +11,55 @@ import SearchBar from '@/components/SearchBar'
 
 const Layout = () => {
   return (
-    <html lang='es'>
-      <body>
-        <Routes>
-          <Route
-            path={routes.home}
-            element={
-              <>
-                <SearchBar />
-                <Header />
-                <Home />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path={routes.category}
-            element={
-              <>
-                <Header />
-                <ItemListContainer />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path={routes.itemDetail}
-            element={
-              <>
-                <Header />
-                <ItemDetailContainer />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path={routes.notFound}
-            element={
-              <>
-                <Header />
-                <NotFound />
-                <Footer />
-              </>
-            }
-          />
-          <Route
-            path={routes.administrationPanel}
-            element={<AdministrationPanel />}
-          />
-        </Routes>
-      </body>
-    </html>
+    <>
+      <Routes>
+        <Route
+          path={routes.home}
+          element={
+            <>
+              <SearchBar />
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path={routes.category}
+          element={
+            <>
+              <Header />
+              <ItemListContainer />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path={routes.itemDetail}
+          element={
+            <>
+              <Header />
+              <ItemDetailContainer />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path={routes.notFound}
+          element={
+            <>
+              <Header />
+              <NotFound />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path={routes.administrationPanel}
+          element={<AdministrationPanel />}
+        />
+      </Routes>
+    </>
   )
 }
 
