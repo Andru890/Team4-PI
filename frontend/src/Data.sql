aab9be4eb3523fbe6d264d46d76bd08b73a63e1c
wdelcant <wilsondelcanto.redes@gmail.com>
2024-05-18 18:00:36 -0400

    refactor: Update npm dependencies for Cloudinary integration

--- frontend/src/Data.sql
+++
@@ -1,4 +0,0 @@
-CONNECTION URL: jdbc:mysql://localhost:3307/visual_studio_db
-NAME: visual_studio_db
-USERNAME: root
-PASSWORD: sa
---
+++ query_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO categories (name) VALUES
+	 ('camaras'),
+	 ('sonido'),
+	 ('luces'),
+	 ('accesorios');
