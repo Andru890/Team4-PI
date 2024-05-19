553b03b5c7ce2ff148dfefbffacba7c70bf7bb59
Wilson Del Canto Cepeda <99770820+wdelcant@users.noreply.github.com>
2024-05-19 00:22:23 -0400

    cors

--- src/main/java/com/visualstudio/rest/api/config/WebCorsConfig.java
+++ src/main/java/com/visualstudio/rest/api/config/WebCorsConfig.java
@@ -11,6 +11,10 @@ public class WebCorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
