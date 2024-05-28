0da45b77896e4142834e2d5f78d36df95620a61d
Wilson Del Canto Cepeda <99770820+wdelcant@users.noreply.github.com>
2024-05-28 01:58:10 -0400

    refactor: Update AuthProvider import in main.jsx to use correct file path

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ContextProvider } from '@/context/global.context.jsx'
-import { AuthProvider } from '@/context/AuthContext'
+import { AuthProvider } from '@/context/auth.context'
 import App from '@/App'
 import '@/index.css'
 import ErrorBoundary from '@/components/ErrorBoundary'
