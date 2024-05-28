e6f19cb1e9c0ee78c320279f723f94eea6198280
PauAlejaP <pa.al.pa.ag12@gmail.com>
2024-05-27 21:13:38 -0500

    RECUPERE ALGUNA COSAS

--- src/main/java/com/visualstudio/rest/api/enums/exceptions/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.visualstudio.rest.api.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);}
-}
