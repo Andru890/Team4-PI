2514c3a55294aae436f76c2976172dffd59b8eaf
PauAlejaP <pa.al.pa.ag12@gmail.com>
2024-05-30 11:26:28 -0500

    CONTROLADOR PARA CREAR USUARIO Y ACTUALIZAR USUARIO

--- backend/src/main/java/com/visualstudio/rest/api/controllers/UserController.java
+++ backend/src/main/java/com/visualstudio/rest/api/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.visualstudio.rest.api.controllers;
 
 import com.visualstudio.rest.api.models.entities.Role;
 import com.visualstudio.rest.api.models.entities.User;
+import com.visualstudio.rest.api.repositories.UserRepository;
 import com.visualstudio.rest.api.services.IUserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,14 @@ import java.util.List;
 public class UserController {
 
     private final IUserService userService;
+    private final UserRepository userRepository;
 
     @PostMapping
     public ResponseEntity<User> userAdd(@Valid @RequestBody User user) {
-
+        User userAdd = userService.save(user);
+        if (userAdd == null || userAdd == userRepository.getReferenceById(user.getId())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
         return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
     }
 
@@ -44,6 +49,7 @@ public class UserController {
     }
     @PutMapping
     public ResponseEntity<User> userUpdate(@Valid @RequestBody User user){
+        User userUpdate = userService.update(user);
         return new ResponseEntity<>(userService.update(user), HttpStatus.OK);
     }
 
