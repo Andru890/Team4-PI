f00fa43d80b9e6d76c137b8978a2833430cfefa6
Wilson Del Canto Cepeda <wilsondelcanto.redes@gmail.com>
2024-05-30 15:46:17 -0400

    :egg: add query init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,11 @@
--- Active: 1715836676560@@127.0.0.1@3307@visual_studio_db
+-- Agrega las categorias
+INSERT INTO categories (id, name, image, description) VALUES
+	 (1, 'Cámaras', 'https://res.cloudinary.com/wilsondelcanto-dev/image/upload/v1717018987/categories/adatfk3bwthx713nbyyc.jpg', 'Camaras de fotos y video.'),
+	 (2, 'Sonido', 'https://res.cloudinary.com/wilsondelcanto-dev/image/upload/v1717019008/categories/p4imbfvzytianq3rjsqx.jpg', 'Equipos de sonido.'),
+	 (3, 'Luces', 'https://res.cloudinary.com/wilsondelcanto-dev/image/upload/v1717019028/categories/pfoeqmaypxsh45debsju.jpg', 'Luces para eventos.'),
+	 (4, 'Accesorios', 'https://res.cloudinary.com/wilsondelcanto-dev/image/upload/v1717018983/categories/l0zanig5xcmuz3crdiex.jpg', 'Accesorios para eventos.');
+
+-- Agrega los productos por defecto
 INSERT INTO products (
 		characteristic,
 		description,
@@ -224,3 +231,11 @@ VALUES (
 		1,
 		3
 	);
+
+-- Agrega el rol admin
+
+INSERT INTO roles (id, name) VALUES (1, 'admin'), (2, 'customer');
+
+-- Agrega el usuario admin
+
+INSERT INTO users (id, name, email, password, role_id) VALUES (1, 'admin', 'admin@visualstudioservice.com', '123123', 1);
--- query_role.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO roles (id, name) VALUES (1, 'admin');
