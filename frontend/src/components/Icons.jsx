ff2e3f1f19508aa06baa02809ee79c98f8ae3111
Wilson Del Canto Cepeda <99770820+wdelcant@users.noreply.github.com>
2024-06-02 10:33:17 -0400

    feat: Add new icons for camera, microphone, and triangle

--- frontend/src/components/Icons.jsx
+++ frontend/src/components/Icons.jsx
@@ -662,7 +662,67 @@ export function PencilIcon(props) {
   )
 }
 
-export function CameraIcon() {
+export function TriangleIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path d='M13.73 4a2 2 0 0 0-3.46 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z' />
+    </svg>
+  )
+}
+
+export function MicIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path d='M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z' />
+      <path d='M19 10v2a7 7 0 0 1-14 0v-2' />
+      <line x1='12' x2='12' y1='19' y2='22' />
+    </svg>
+  )
+}
+
+export function CameraIcon(props) {
+  return (
+    <svg
+      {...props}
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path d='M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z' />
+      <circle cx='12' cy='13' r='3' />
+    </svg>
+  )
+}
+
+export function CameraLogo() {
   return (
     <svg
       viewBox='0 0 1024 1024'
