a9556feb7a16cf48e921f3519df6ecf466fc15f1
PauAlejaP <pa.al.pa.ag12@gmail.com>
2024-06-03 17:24:40 -0500

    CREACION DE SEGURIDAD RESOLVIENDO EL PROBLEMA DEL PUSH version2

--- backend/src/main/java/com/visualstudio/rest/api/VisualStudioApplication.java
+++ backend/src/main/java/com/visualstudio/rest/api/VisualStudioApplication.java
@@ -1,28 +1,8 @@
 package com.visualstudio.rest.api;
 
 
-import com.visualstudio.rest.api.models.entities.Product;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.visualstudio.rest.api.models.entities.Role;
-import com.visualstudio.rest.api.models.entities.User;
-import com.visualstudio.rest.api.repositories.UserRepository;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationContextInitializedEvent;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
-
-
-
 
 @SpringBootApplication
 public class VisualStudioApplication {
--- backend/src/main/java/com/visualstudio/rest/api/configuration/SecurityConfig.java
+++ backend/src/main/java/com/visualstudio/rest/api/configuration/SecurityConfig.java
@@ -74,11 +74,5 @@ public class SecurityConfig {
 
         return http.build();
     }
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception
-    { return authenticationConfiguration.getAuthenticationManager();}
 
-    @Bean
-    public PasswordEncoder passwordEncoder()
-    { return new BCryptPasswordEncoder(); }
-}
+    }
