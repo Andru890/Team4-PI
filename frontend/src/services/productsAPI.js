dd7c2d84bfae3827e837b377af5c33e982d7c7d3
Wilson Del Canto Cepeda <99770820+wdelcant@users.noreply.github.com>
2024-06-02 22:47:49 -0400

    refactor: Update API URLs to use import.meta.env.VITE_API_URL

--- frontend/src/services/productsAPI.js
+++ frontend/src/services/productsAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = import.meta.env.VITE_API_URL_DEVELOPMENT
+const API_URL = import.meta.env.VITE_API_URL
 
 export const getProduct = async () => {
   try {
