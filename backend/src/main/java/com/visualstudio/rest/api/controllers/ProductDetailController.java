6eac18eb87f269cadb5b989cb6a8987d259cd253
Eliana Sanchez <116673136+elisanchezp4@users.noreply.github.com>
2024-06-03 20:27:14 -0500

    Se agrega endpoints para caracteristicas, y excepciones

---
+++ backend/src/main/java/com/visualstudio/rest/api/controllers/ProductDetailController.java
@@ -0,0 +1,85 @@
+package com.visualstudio.rest.api.controllers;
+
+import com.visualstudio.rest.api.models.dtos.ProductDetailDTO;
+import com.visualstudio.rest.api.models.entities.ProductDetail;
+import com.visualstudio.rest.api.services.IProductDetailService;
+import com.visualstudio.rest.api.services.impl.CloudinaryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/product-detail")
+@RequiredArgsConstructor
+public class ProductDetailController {
+
+    private final IProductDetailService productDetailService;
+    private final CloudinaryService cloudinaryService;
+
+    @Operation(summary = "Create a productDetail")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Create a productDetail",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProductDetail.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid productDetail id", content = @Content)})
+    @PostMapping("/product/{productId}")
+    public ResponseEntity<ProductDetailDTO> addProductDetail(@RequestBody ProductDetail productDetail, @PathVariable Long productId) {
+        return new ResponseEntity<>(productDetailService.save(productDetail, productId), HttpStatus.CREATED);
+    }
+
+
+    @Operation(summary = "Update a productDetail")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Update a productDetail",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProductDetail.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid productDetail id", content = @Content)})
+    @PutMapping("/{productDetailId}")
+    public ResponseEntity<ProductDetailDTO> updateProductDetail(@RequestBody ProductDetail productDetail, @PathVariable Long productDetailId) {
+        return new ResponseEntity<>(productDetailService.update(productDetail, productDetailId), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Found list productDetail")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found a productDetail",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProductDetail.class))}),
+            @ApiResponse(responseCode = "404", description = "ProductDetail not found", content = @Content)})
+    @GetMapping
+    public ResponseEntity<List<ProductDetailDTO>> searchAll() {
+        return new ResponseEntity<>(productDetailService.getAll(), HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "Found a productDetail")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found a productDetail",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProductDetail.class))}),
+            @ApiResponse(responseCode = "404", description = "ProductDetail not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid productDetail id", content = @Content)})
+    @GetMapping("/{productDetailId}")
+    public ResponseEntity<ProductDetailDTO> detailProductDetail(@PathVariable Long productDetailId) {
+        return new ResponseEntity<>(productDetailService.findById(productDetailId), HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "Delete a productDetail")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Deleted a productDetail",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ProductDetail.class))}),
+            @ApiResponse(responseCode = "404", description = "ProductDetail not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Invalid productDetail id", content = @Content)})
+    @DeleteMapping("/{productDetailId}")
+    // Service de imagen para borrar
+    public ResponseEntity<Void> delete(@PathVariable Long productDetailId) {
+        productDetailService.delete(productDetailId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
+
+
--- backend/src/main/java/com/visualstudio/rest/api/services/impl/CategoryServiceImpl.java
+++ backend/src/main/java/com/visualstudio/rest/api/services/impl/CategoryServiceImpl.java
@@ -1,5 +1,6 @@
 package com.visualstudio.rest.api.services.impl;
 
+import com.visualstudio.rest.api.exceptions.ResourceNotFoundException;
 import com.visualstudio.rest.api.models.dtos.CategoryDTO;
 import com.visualstudio.rest.api.models.entities.Category;
 import com.visualstudio.rest.api.repositories.CategoryRepository;
@@ -32,12 +33,18 @@ public class CategoryServiceImpl implements ICategoryService {
     }
 
     @Override
-    public CategoryDTO update(Category category, Long id) {
-        return null;
+    public Category update(Category category, Long id) {
+        Category categoryFound = categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("No existe Categoria con id %s", id)));
+        categoryFound.setName(category.getName());
+        categoryFound.setDescription(category.getDescription());
+        categoryFound.setImage(category.getImage());
+        return categoryRepository.save(categoryFound);
     }
     @Override
     public CategoryDTO findById(Long id) {
-        return convertToDTO(categoryRepository.findById(id).get());
+        return convertToDTO(categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("No existe Categoria con id %s" , id))));
     }
 
     @Override
