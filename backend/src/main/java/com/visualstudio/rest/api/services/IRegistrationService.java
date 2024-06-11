e37e64c8ddb6d255786f67ab8f7663c49147e227
PauAlejaP <pa.al.pa.ag12@gmail.com>
2024-06-11 09:34:57 -0500

    VERSION DEL 11 DE JUNIO 9:34 am.

---
+++ backend/src/main/java/com/visualstudio/rest/api/services/IRegistrationService.java
@@ -0,0 +1,16 @@
+package com.visualstudio.rest.api.services;
+
+import com.visualstudio.rest.api.dto.LoginDto;
+import com.visualstudio.rest.api.dto.RegistroDto;
+import com.visualstudio.rest.api.models.entities.Role;
+import com.visualstudio.rest.api.models.entities.User;
+
+public interface IRegistrationService {
+
+    User save(RegistroDto registroDto);
+
+    User confirmRegistration(String email);
+    String authenticate(LoginDto loginDto);
+
+    Role getDefaultRole();
+}
