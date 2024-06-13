ce848246b884dcbe39817fc6af4402169bf42433
Wilson Del Canto Cepeda <99770820+wdelcant@users.noreply.github.com>
2024-06-13 00:58:28 -0400

    refactor: Update image URLs and upload functionality in FavsList and ItemDetail components

--- frontend/src/components/FavsList/FavsList.jsx
+++ frontend/src/components/FavsList/FavsList.jsx
@@ -1,5 +1,52 @@
+import { useGlobalContext } from '@/context/global.context'
+import { useEffect } from 'react'
+import ItemList from '@/components/ItemListContainer/ItemList'
+
 const FavsList = () => {
-  return <h1>Favlsit</h1>
+  const { state, handleClearFavs, handleGetFavs } = useGlobalContext()
+
+  useEffect(() => {
+    handleGetFavs()
+  }, [handleGetFavs])
+
+  const productList = state.favs || []
+
+  return (
+    <section className='w-full flex flex-col items-center justify-center gap-4 p-4'>
+      <h1 className='font-semibold text-lg md:text-2xl text-center'>
+        Favoritos
+      </h1>
+      <p className='text-center'>
+        Aquí podrás ver tus productos favoritos y comprarlos cuando quieras.
+      </p>
+      {productList.length > 0 && (
+        <button
+          className='clearButton btn btn-secondary'
+          onClick={handleClearFavs}
+        >
+          Borrar lista de favoritos
+        </button>
+      )}
+      <div className='w-full'>
+        {productList.length > 0 ? (
+          <div className='container grid grid-cols-1 md:grid-cols-2 gap-6 px-4 md:px-6 mt-5'>
+            <ItemList productList={productList} />
+          </div>
+        ) : (
+          <div className='flex flex-col items-center'>
+            <div className='flex flex-col items-center justify-center gap-4'>
+              <img
+                src='/images/empty-favs.jpg'
+                alt='Favoritos vacíos'
+                className='w-64 h-64'
+              />
+              <button className='btn btn-primary'>Explorar productos</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </section>
+  )
 }
 
 export default FavsList
--- frontend/src/pages/Favs.jsx
+++ frontend/src/pages/Favs.jsx
@@ -1,56 +1,13 @@
-import { useGlobalContext } from '@/context/global.context'
-import { useEffect } from 'react'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
-import ItemList from '@/components/ItemListContainer/ItemList'
 import FavsList from '@/components/FavsList/FavsList'
 
 const Favs = () => {
-  const { state, handleClearFavs, handleGetFavs } = useGlobalContext()
-
-  useEffect(() => {
-    handleGetFavs()
-  }, [handleGetFavs])
-
-  const productList = state.favs || []
-
   return (
     <>
       <Header />
-      <main className='flex flex-1 flex-col items-center justify-center gap-4 p-4 mt-20'>
-        <section className='flex flex-1 flex-col items-center justify-center gap-4 p-4'>
-          <h1 className='font-semibold text-lg md:text-2xl'>Favoritos</h1>
-          {productList.length > 0 && (
-            <button className='clearButton' onClick={handleClearFavs}>
-              Clear favorites list
-            </button>
-          )}
-          <div className='card-grid'>
-            {productList.length > 0 ? (
-              <ItemList productList={productList} />
-            ) : (
-              <div>
-                <p className='text-center'>
-                  Aquí podrás ver tus productos favoritos y comprarlos cuando
-                  quieras.
-                </p>
-                <div className='flex flex-col items-center justify-center gap-4'>
-                  <img
-                    src='/images/empty-favs.jpg'
-                    alt='Favoritos vacíos'
-                    className='w-64 h-64'
-                  />
-                  <button className='btn btn-primary'>
-                    Explorar productos
-                  </button>
-                </div>
-              </div>
-            )}
-          </div>
-        </section>
-        <section>
-          <FavsList />
-        </section>
+      <main className='flex flex-col items-center justify-center gap-4 p-4 mt-20'>
+        <FavsList />
       </main>
       <Footer />
     </>
