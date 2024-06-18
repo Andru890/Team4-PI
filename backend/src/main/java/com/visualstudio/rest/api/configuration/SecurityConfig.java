95dc8412173e6aa814c636d56c7155fe1a7a152c
Camilo Patiño <nelsoncamilopatino@gmail.com>
2024-06-17 21:52:42 -0300

    Sumando productDetail a la whitelist de acceso

--- backend/src/main/java/com/visualstudio/rest/api/configuration/SecurityConfig.java
+++ backend/src/main/java/com/visualstudio/rest/api/configuration/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
             "product/**",
             "v1/**",
             "registration/**",
-            "confirmation-email/**"}
+            "confirmation-email/**",
+            "product-detail/**"}
             ;
 
 
