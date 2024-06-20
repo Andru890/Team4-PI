223a99871d63dacec74852714e792ee8e7404e84
Camilo Patiño <nelsoncamilopatino@gmail.com>
2024-06-20 10:46:52 -0300

    Fix change rol

--- backend/src/main/java/com/visualstudio/rest/api/services/impl/UserServiceImpl.java
+++ backend/src/main/java/com/visualstudio/rest/api/services/impl/UserServiceImpl.java
@@ -97,7 +97,19 @@ public class UserServiceImpl implements IUserService {
 
     @Override
     public User updateRole(Long userId) {
-        return null;
+        User user = userRepository.findById(userId).get();
+        Role newRole = roleRepository.findByName("admin");
+
+        if (newRole == null){
+            updateRole(userId);
+        }
+
+        if (user.getRole().getName() == "customer"){
+            user.setRole(newRole);
+        } else if (user.getRole().getName() == "admin") {
+            user.setRole(roleRepository.findByName("customer"));
+        }
+        return userRepository.save(user);
     }
 
     public FavoriteProducts addFavorite(Long userId, Long productId) {
