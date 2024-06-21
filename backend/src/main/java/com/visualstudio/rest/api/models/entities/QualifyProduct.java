cbc5bcfe87d13d15a2219135b6b261b539a56d63
Camilo Patiño <nelsoncamilopatino@gmail.com>
2024-06-20 21:33:20 -0300

    Entity qualify

---
+++ backend/src/main/java/com/visualstudio/rest/api/models/entities/QualifyProduct.java
@@ -0,0 +1,31 @@
+package com.visualstudio.rest.api.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "qualify_product")
+@Builder(toBuilder = true)
+public class QualifyProduct {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @OneToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+}
