2374fe680e311f731bb70c62d030e53de2e4f3ec
Camilo Patiño <nelsoncamilopatino@gmail.com>
2024-06-20 22:50:37 -0300

    Cambio en favoritos para guardar por mail

--- backend/src/main/java/com/visualstudio/rest/api/services/impl/FavoriteProductsService.java
+++ backend/src/main/java/com/visualstudio/rest/api/services/impl/FavoriteProductsService.java
@@ -35,10 +35,10 @@ public class FavoriteProductsService  implements IFavoriteProductsService {
         return products;
     }
     @Override
-    public void saveFavorite(Long userId, Long productId){
-        User user = userRepository.findById(userId).get();
+    public void saveFavorite(String userEmail, Long productId){
+        User user = userRepository.findByEmail(userEmail);
         Product product = productsRepository.findById(productId).get();
-        List<FavoriteProducts> existingProducts = favoriteProductsRepository.findByUserIdAndProductId(userId, productId);
+        List<FavoriteProducts> existingProducts = favoriteProductsRepository.findByUserIdAndProductId(user.getId(), productId);
         if (!existingProducts.isEmpty()) {
             throw new IllegalArgumentException("El producto ya es favorito");
         }
