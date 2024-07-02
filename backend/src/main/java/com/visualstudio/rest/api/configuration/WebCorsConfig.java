fbc5c7026a1394395cdfe8dac870d5c63ee5a14c
Camilo Patiño <nelsoncamilopatino@gmail.com>
2024-07-02 11:21:24 -0300

    cors add addres https://visualstudioservice.pro

--- backend/src/main/java/com/visualstudio/rest/api/configuration/WebCorsConfig.java
+++ backend/src/main/java/com/visualstudio/rest/api/configuration/WebCorsConfig.java
@@ -14,7 +14,7 @@ public class WebCorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                 .allowedOrigins("http://localhost:3000/", "http://visualstudioservice.duckdns.org:3000")
+                 .allowedOrigins("http://localhost:3000/", "http://visualstudioservice.duckdns.org:3000", "https://visualstudioservice.pro")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
